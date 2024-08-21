f8c7d8b2f3acf02b9b94fb3f7b6695d324952f30
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-08-22 00:27:51 +0100

    Client Section completed

---
+++ components/Clients.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards';
+import { companies, testimonials } from '@/data';
+
+const Clients = () => {
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        Kind Words From
+        <span className="text-purple"> Colleagues</span>
+      </h1>
+      <div className="flex flex-col mt-5 items-center max-lg:mt-10">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+          {companies.map(({ id, img, name, nameImg }) => (
+            <div id={id} className="flex md:max-w-60 max-w-32 gap-2">
+              <img src={img} alt={name} className="md:w-10 w-5" />
+              <img
+                src={nameImg}
+                alt={name}
+                className="md:w-24 w-20"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clients;
